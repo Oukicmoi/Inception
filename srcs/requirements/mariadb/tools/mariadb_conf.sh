fb6c86f71a9e2aac2a6adcc504d73288ffa33487
gtraiman <gtraiman@gtraiman.myguest.virtualbox.org>
2025-07-02 21:01:58 +0200

    add comments on mariadb_conf

--- srcs/requirements/mariadb/tools/mariadb_conf.sh
+++ srcs/requirements/mariadb/tools/mariadb_conf.sh
@@ -19,24 +19,26 @@ while [ ! -S /run/mysqld/mysqld.sock ]; do
 done
 echo "✅ MariaDB is ready"
 
-echo "1"
+#change le mdp de root pour le set a $SQL_ROOT_PASS 
 mysql -u root -p"$SQL_ROOT_PASS" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASS}'"
-echo "15"
+
+#cree la db
 mysql -u root -p"$SQL_ROOT_PASS" -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE"
 
-echo "2"
+#cree le user
 mysql -u root -p"$SQL_ROOT_PASS" -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASS';"
 
-echo "3"
+#accorde tout les droits au user sur la db
 mysql -u root -p"$SQL_ROOT_PASS" -e "GRANT ALL ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASS';"
 
-echo "4"
+#redefinis le mdp avec le mdp actuel pour le confirmer
 mysql -u root -p"$SQL_ROOT_PASS" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASS';"
 
-echo "5"
+#active les modifications (autoomatiquement mises en caches par mysql)
 mysql -u root -p"$SQL_ROOT_PASS" -e "flush privileges;"
 
-echo "6"
+#eteint proprement le serveur mariadb
 mysqladmin -u root -p"$SQL_ROOT_PASS" shutdown
 
+#commande bash pour lancer mariadb en tant que processus principal du container/service
 exec mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
